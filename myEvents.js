40fc00a644d16eb297b93402a7174e81ff07513d
teoespero <tespero@wgu.edu>
2018-08-23 16:53:15 -0700

    we're using regex now

--- myEvents.js
+++ myEvents.js
@@ -1,3 +1,9 @@
+// 	Teo Espero
+// 	BS Cloud and Systems Administration
+// 	Western Governors University
+
+// 	#Web-Dev-Bootcamp-Udemy
+
 var letters = 0;	
 var numbers = 0;
 var specialChars = 0;
@@ -20,30 +26,48 @@ $("#letra").keypress(function(key){
 	var whatChar = String.fromCharCode(key.which);
 	var key = key.which;
 
+	if (/[a-z]/.test(whatChar.toLowerCase())){
+		letters++;
+	}
+	else if (/^\d+$/.test(whatChar)){
+		numbers++;
+	}
+	else {
+		specialChars++;
+	}
+	
+	///////////////////////////////////////////////////////////	
+	// below is the straight javascript method 
+	// of getting the keypress() stats
+	// we're using regex because it looks cooler
+	///////////////////////////////////////////////////////////
 
-	$("#whatKey").html(whatChar);
-	if (key >= 48 && key <= 57) {
+	// $("#whatKey").html(whatChar);
+	// if (key >= 48 && key <= 57) {
 	    
-	    // Number
-	    $("#charType").html("Number");
-	    numbers++;
-	} else if (key >= 65 && key <= 90) {
+	//     // Number
+	//     $("#charType").html("Number");
+	//     numbers++;
+	// } else if (key >= 65 && key <= 90) {
 	    
-	    // Alphabet upper case
-	    $("#charType").html("Uppercase letter");
-	    letters++;
-	} else if (key >= 97 && key <= 122) {
+	//     // Alphabet upper case
+	//     $("#charType").html("Uppercase letter");
+	//     letters++;
+	// } else if (key >= 97 && key <= 122) {
 	    
-	    // Alphabet lower case
-	    $("#charType").html("Lowercase letter");
-	    letters++;
-	}
-	else{
+	//     // Alphabet lower case
+	//     $("#charType").html("Lowercase letter");
+	//     letters++;
+	// }
+	// else{
 		
-		// Special Character
-	    $("#charType").html("Special character");	
-	    specialChars++;
-	}
+	// 	// Special Character
+	//     $("#charType").html("Special character");	
+	//     specialChars++;
+	// }
+	///////////////////////////////////////////////////////////
+
+	// show the stats
 	$("#letter").html(letters);
 	$("#number").html(numbers);
 	$("#special").html(specialChars);
